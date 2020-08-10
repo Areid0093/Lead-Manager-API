e5d2894344cc004c9cb448fbf5905af02d5c8a45
Alec Reid <alecreid0093@gmail.com>
2020-08-09 19:06:51 -0500

    Error Handler added to actions

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -4,9 +4,13 @@ from rest_framework import viewsets, permissions
 
 
 class LeadViewSet(viewsets.ModelViewSet):
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
     serializer_class = LeadSerializer
 
+    def get_queryset(self):
+        return self.request.user.leads.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
